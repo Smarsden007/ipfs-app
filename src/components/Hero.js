0b60afd2fc9a4a892512e2fbc67fcdef380ed071
Smarsden007 <seannmarsden@gmail.com>
2023-01-15 22:28:10 -0800

    Added text spacing and color gradient background.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export const Hero = () => {
   return (
-    <div className='flex flex-col justify-center w-full p-10 bg-gradient-pink-lightblue'>
-    <div className='w-full mx-auto'>
-        <div className='mx-auto text-left w-7/12'>
-            <h1 className='text-7xl text-bolder font-sans' style={{fontWeight:'bolder'}}>Building The Next Generation of Crypto Research & Trading Technologies, <span style={{fontWeight:'lighter'}}> at scale</span></h1>
-            <h2 className='text-2xl'>
-                We're a 6 years old crypto hedge fund that focuses on systematic tradic and HTF,<span> on CEX & DEX</span>
-            </h2>
+    <div className="flex flex-col justify-center w-full p-10 bg-gradient-to-b from-pink-600 to-blue-200">
+      <div className="w-full mx-auto">
+        <div className="mx-auto text-left w-7/12">
+          <h1
+            className="text-7xl text-bolder font-sans leading-[5.25rem]"
+            style={{ fontWeight: "bolder" }}
+          >
+            Building The Next Generation of Crypto Research & Trading
+            Technologies,{" "}
+            <span
+              style={{ fontWeight: "lighter", background: "black" }}
+              className="bg-black-900 text-white"
+            >
+              {" "}
+              at scale
+            </span>
+          </h1>
+          <div className="w-6/12">
+          <h2 className="text-2xl">
+            We're a 6 years old crypto hedge fund that focuses on systematic
+            tradic and HTF,<span> on CEX & DEX</span>
+          </h2>
+          </div>
         </div>
-        <div className='mx-auto text-left w-96'>
-            
-        </div>
+        <div className="mx-auto text-left w-96"></div>
+      </div>
     </div>
-</div>
-
-  )
-}
+  );
+};
